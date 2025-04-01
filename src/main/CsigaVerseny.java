c89d6cf76c76f645185d6b13ef227c3af1a71525
S4mur419020 <muraitomi04@gmail.com>
2025-04-01 08:32:09 +0200

    javítva

--- src/main/CsigaVerseny.java
+++ src/main/CsigaVerseny.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
 public class CsigaVerseny {
     private static final Random RND=new Random();
     private static final Scanner SC=new Scanner(System.in);
-    private static int MIN=0;
-    private static int MAX=3;
+    private static final int MIN=0;
+    private static final int MAX=3;
     private Csiga[] csigak;
     private  int[] AktPozTomb;
-    private final int KOROK_SZAMA = 10;
+    private static final int KOROK_SZAMA = 10;
  
 
     public CsigaVerseny(){
@@ -32,18 +32,19 @@ public class CsigaVerseny {
 
     void Start() {
         
-        
+        int legtav=0;
+        int nyertesi=0;
         int[] AktPozTomb=new int[csigak.length];
         System.out.println("Melyik csiga fog nyerni szerinted?:");
         String tipp=SC.nextLine();
         System.out.print("Majd kiderül");
-        int legtav=0;
-        int nyertesi=0;
+        
         for (int i = 0; i < KOROK_SZAMA; i++) {
             System.out.printf("%d. kör:\n",i+1);
             for (int j = 0; j < csigak.length; j++) {
                 AktPozTomb[j]+=csigak[j].Gyorsito();
-                System.out.println(" "+ "Aktuális pozicio: "+AktPozTomb[j]+" "+csigak[j].getSzkod()+" "+csigak[j].getIkon());
+                System.out.println(" "+ "Aktuális pozicio: "+AktPozTomb[j]+
+                        " "+csigak[j].getSzkod()+" "+csigak[j].getIkon());
                 if (AktPozTomb[j]>legtav) {
                     legtav=AktPozTomb[j];
                     nyertesi=j;
@@ -51,10 +52,11 @@ public class CsigaVerseny {
                 }
             }
         }
-        System.out.println("A nyertes: "+AktPozTomb[nyertesi]+" "+"a távolságal"+csigak[nyertesi].getSzkod()+" "+csigak[nyertesi].getIkon());
+        System.out.println("A nyertes: "+AktPozTomb[nyertesi]+
+                " "+"távolságal"+csigak[nyertesi].getSzkod()+" "+csigak[nyertesi].getIkon());
         
         
-        boolean vanNyertes = tipp==csigak[0].getSzin()||tipp==csigak[1].getSzin()||tipp==csigak[2].getSzin();
+        boolean vanNyertes = tipp == csigak[0].getSzin()||tipp == csigak[1].getSzin()||tipp == csigak[2].getSzin();
   
         if (vanNyertes) {
             System.out.println("Eltaláltad, nyertél!");
